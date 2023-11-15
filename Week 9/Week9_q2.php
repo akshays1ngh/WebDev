aab8380441d4cc853c73281f019c277de589832a
Akshay Singh <96130904+akshays1ngh@users.noreply.github.com>
2023-11-15 07:51:53 +0530

    Update Week9_q2.php

--- Week 9/Week9_q2.php
+++ Week 9/Week9_q2.php
@@ -3,62 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resume Upload</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin: 50px;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        input {
-            margin-bottom: 10px;
-        }
-    </style>
+    <title>Last Visited Time</title>
 </head>
 <body>
 
-<h2>Upload Your Resume</h2>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <label for="resume">Select PDF Resume:</label>
-    <input type="file" name="resume" accept=".pdf" required>
-    <br>
-    <input type="submit" value="Upload">
-</form>
-
 <?php
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if the file was uploaded without errors
-    if (isset($_FILES["resume"]) && $_FILES["resume"]["error"] == 0) {
-        $targetDir = "uploads/";  // Specify the directory where you want to store the resumes
-
-        // Generate a unique filename based on the current timestamp
-        $targetFile = $targetDir . time() . "_" . basename($_FILES["resume"]["name"]);
-
-        // Check if the file is a PDF
-        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-        if ($fileType == "pdf") {
-            // Move the uploaded file to the specified directory
-            if (move_uploaded_file($_FILES["resume"]["tmp_name"], $targetFile)) {
-                echo "<p style='color: green;'>Resume uploaded successfully!</p>";
-            } else {
-                echo "<p style='color: red;'>Error uploading the resume.</p>";
-            }
-        } else {
-            echo "<p style='color: red;'>Please upload a PDF file.</p>";
-        }
-    } else {
-        echo "<p style='color: red;'>Error uploading the resume.</p>";
-    }
+// Function to set a cookie with the current date-time
+function setLastVisitedCookie() {
+    $lastVisited = date('Y-m-d H:i:s');
+    setcookie('last_visited', $lastVisited, time() + (86400 * 30), "/"); // Cookie expires in 30 days
 }
+
+// Check if the 'last_visited' cookie is set
+if(isset($_COOKIE['last_visited'])) {
+    $lastVisitedTime = $_COOKIE['last_visited'];
+    echo "<p>Last visited on: $lastVisitedTime</p>";
+} else {
+    echo "<p>Welcome! This is your first visit.</p>";
+}
+
+// Set the 'last_visited' cookie with the current date-time
+setLastVisitedCookie();
 ?>
 
 </body>
